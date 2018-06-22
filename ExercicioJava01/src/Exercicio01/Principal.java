d6598fa1ddb8ecd614b6578c188561097bd47e2f
Larysson da Silva Nascimento <data52691@datainfo.inf.br>
2018-06-22 11:25:34 -0300

    Finalizando Exercicio

--- ExercicioJava01/src/Exercicio01/Principal.java
+++ ExercicioJava01/src/Exercicio01/Principal.java
@@ -1,86 +1,11 @@
 package Exercicio01;
 
-import java.util.Scanner;
+
 
 public class Principal {
-	
-	static Scanner teclado = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Usuario[] usuarios = new Usuario[100];
-		int op = 0;
-		
-		usuarios[0] = new Usuario("Larysson", 52691, "Larysson_cb", "Rua 25","52912");
-		usuarios[1] = new Usuario("Luiz", 5245691, "Lauiz@10", "Rua 28","545612");
-		usuarios[2] = new Usuario("Luiza", 57791, "Lauiza@580", "Rua 28","47111");
-		usuarios[3] = new Usuario("Laura", 5741, "Laura@5454", "Rua 48","55844");
-		
-
-		System.out.println("----------------------------------------------------------------------------------------");
-		escreverMenu(op,usuarios);
-		
-	}
-
-	public static void escreverMenu(int op,  Usuario[] usuarios) {
-		System.out.println("Tela Menu Principal");
-		System.out.println("----------------------------------------------------------------------------------------");
-		System.out.println("Menu Principal");
-		System.out.println("1 - Incluir Usuário");
-		System.out.println("2 - Alterar Usuário");
-		System.out.println("3 - Excluir Usuário");
-		System.out.println("4 - Consultar Usuário");
-		System.out.println("5 - Listar Usuários");
-		System.out.println("9 - Sair do Sistema");
-		System.out.print("Digite a opção desejada:");
-		op = teclado.nextInt();
-		System.out.println("----------------------------------------------------------------------------------------");
-		selecionarOpMenu(op, usuarios);
-	}
-
-	public static void selecionarOpMenu(int op, Usuario[] usuarios) {
-		switch (op) {
-			case 1:
-				System.out.println("Tela: Incluir Usuário");
-				System.out.println("----------------------------------------------------------------------------------------");
-				for(int x =0;x < usuarios.length;x++){
-					if(usuarios[x] == null){
-						usuarios[x] = new Usuario();
-						usuarios[x].cadastrarUsuario();
-						System.out.println(usuarios[x]);
-						break;
-					}
-				}		
-				System.out.println("----------------------------------------------------------------------------------------");
-				escreverMenu(op, usuarios);
-			break;
-			case 2:
-				System.out.println("Tela: Alterar Usuário");
-				System.out.println("----------------------------------------------------------------------------------------");
-				Usuario.alterarUsuario(usuarios);
-				escreverMenu(op, usuarios);
-				break;
-			case 3:
-				 Usuario.excluiUsuario(usuarios);
-				 escreverMenu(op, usuarios);
-				break;
-			case 4:
-				Usuario.consultarDados(usuarios);
-				escreverMenu(op, usuarios);
-				break;
-			case 5:
-				int x =0;
-				for(;x< usuarios.length;x++){
-					if(usuarios[x] != null){
-						System.out.println("----------------------------------------------------------------------------------------");
-						Usuario.exibirUsuarios(usuarios[x]);
-					}
-				}
-				escreverMenu(op, usuarios);
-				break;
-				default:
-					System.out.println("Opção Inválida");
-					escreverMenu(op, usuarios);
-					System.out.println("----------------------------------------------------------------------------------------");
-		}
+		UsuarioRepository.carregarUsuario();
+		Tela.escreverMenu();
 	}
 }
--- ExercicioJava01/src/Exercicio01/Usuario.java
+++ ExercicioJava01/src/Exercicio01/Usuario.java
@@ -1,157 +1,40 @@
 package Exercicio01;
 
-import java.util.Formatter;
 import java.util.Scanner;
 
 public class Usuario {
 	private String nome;
-	private int codigo;
+	private  int codigo;
 	private String login;
 	private String endereco;
 	private String senha;
 
-	static Scanner teclado = new Scanner(System.in);
-
-	public Usuario(){}
 	
-	public Usuario(String nome, int codigo,String login,String endereco,String senha){
+
+	public Usuario() {
+		
+	}
+
+	public Usuario(String nome,int cod,String login, String endereco, String senha) {
 		this.nome = nome;
-		this.codigo = codigo;
 		this.login = login;
 		this.endereco = endereco;
 		this.senha = senha;
+		this.codigo  = codigo;
+		
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		String s = "Nome: " + nome;
 		s += "\nCódigo: " + codigo;
 		s += "\nLogin: " + login;
+		s += "\nEndereço: " + endereco;
 		s += "\nSenha: " + senha;
 
 		return s;
 	}
-
-	public void cadastrarUsuario() {
-		System.out.println("Informe o Nome do Usuário");
-		nome = teclado.next();
-		System.out.println("Informe o Codigo do Usuário");
-		codigo = Integer.parseInt(teclado.next());
-		System.out.println("Informe o Endereço do Usuário");
-		endereco = teclado.next();
-		teclado = new Scanner(System.in);
-		System.out.println("Informe o Login do Usuário");
-		login = teclado.next();
-		System.out.println("Informe a Senha do Usuário");
-		senha = teclado.next();
-		System.out.println("----------------------------------------------------------------------------------------");
-
-	}
-
-	public static void alterarUsuario(Usuario[] usuarios) {
-		System.out.println("Digite o código do Usuario");
-		int cod = teclado.nextInt();
-		System.out.println("----------------------------------------------------------------------------------------");
-		for (int x = 0; x < usuarios.length; x++) {
-			if (usuarios[x] != null) {
-				if (usuarios[x].codigo == cod) {
-					System.out.println(usuarios[x]);
-					System.out.println("----------------------------------------------------------------------------------------");
-
-					System.out.println("Alterando Dados");
-					System.out.println("----------------------------------------------------------------------------------------");
-
-					Usuario usuario2 = new Usuario();
-					usuario2.cadastrarUsuario();
-					System.out.println(usuario2);
-					System.out.println("----------------------------------------------------------------------------------------");
-
-					System.out.println("Mensagem: 1) Voltar, 2) Confirmar");
-					if (Integer.parseInt(teclado.next()) == 2) {
-						usuarios[x] = usuario2;
-						System.out.println("----------------------------------------------------------------------------------------");
-
-						System.out.println("Usuário Alterado");
-						System.out.println("----------------------------------------------------------------------------------------");
-
-						System.out.println(usuarios[x]);
-						System.out.println("----------------------------------------------------------------------------------------");
-
-						break;
-					} else {
-						System.out.println("Operação cancelada");
-					}
-
-				}
-			}
-			if (x == usuarios.length - 1) {
-				System.out.println("Usuário não cadastrado");
-				System.out.println("----------------------------------------------------------------------------------------");
-			}
-		}
-	}
-
-	public static void excluiUsuario(Usuario[] usuarios) {
-		System.out.println("Digite o código do Usuario");
-		int cod = teclado.nextInt();
-		System.out.println("----------------------------------------------------------------------------------------");
-		for (int x = 0; x < usuarios.length; x++) {
-			if (usuarios[x] != null) {
-				if (usuarios[x].codigo == cod) {
-					System.out.println("Deseja Excluir Usuario?");
-					System.out.println("----------------------------------------------------------------------------------------");
-					System.out.println(usuarios[x]);
-					System.out.println("----------------------------------------------------------------------------------------");
-					System.out.println("Mensagem: 1) Voltar, 2) Confirmar");
-					if (Integer.parseInt(teclado.next()) == 2){
-						System.out.println("----------------------------------------------------------------------------------------");
-						System.out.println("Usuário Excluido");
-						System.out.println("----------------------------------------------------------------------------------------");
-						usuarios[x] = null;
-						break;
-					} else {
-						System.out.println("Operação cancelada");
-					}
-				}
-			}
-			if (x == usuarios.length - 1) {
-				System.out.println("Usuário não cadastrado");
-				System.out.println("----------------------------------------------------------------------------------------");
-			}
-
-		}
-
-	}
-	public static void consultarDados(Usuario[] usuarios){
-		System.out.println("Digite o código do Usuario");
-		int cod = teclado.nextInt();
-		System.out.println("----------------------------------------------------------------------------------------");
-		for (int x = 0; x < usuarios.length; x++) {
-			if (usuarios[x] != null) {
-				if (usuarios[x].codigo == cod) {
-					System.out.println("Tela Consultar Usuario");
-					System.out.println("----------------------------------------------------------------------------------------");
-					System.out.println(usuarios[x]);
-					System.out.println("----------------------------------------------------------------------------------------");
-					break;
-				}
-			}
-			if (x == usuarios.length - 1) {
-				System.out.println("Usuário não cadastrado");
-				System.out.println("----------------------------------------------------------------------------------------");
-			}
-
-		}
-			
-				
-	}
-	public static void  exibirUsuarios(Usuario usuario){
-				
-		
-	System.out.println(new Formatter().format("|%d|%d|%f|%f|%s|%s|%tc|", usuario)); 
-	}
-
+	
 	public String getNome() {
 		return nome;
 	}
@@ -183,6 +66,7 @@ public class Usuario {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public String getEndereco() {
 		return endereco;
 	}
